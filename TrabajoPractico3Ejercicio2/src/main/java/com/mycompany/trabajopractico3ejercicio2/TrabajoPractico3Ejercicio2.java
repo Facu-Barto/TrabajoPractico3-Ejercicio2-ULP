5e0dc654cbccaf08ca39a0d2f351f7b30f0b182e
facubarto05 <facubarto2005@gmail.com>
2026-09-03 20:19:36 -0300

    Archivo Inicial

---
+++ TrabajoPractico3Ejercicio2/src/main/java/com/mycompany/trabajopractico3ejercicio2/TrabajoPractico3Ejercicio2.java
@@ -0,0 +1,7 @@
+package com.mycompany.trabajopractico3ejercicio2;
+
+public class TrabajoPractico3Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
